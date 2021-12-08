2a40f93b43640cccf941834bfff5d87dbc6976be
Ryan John Lee <rjl@eiva.com>
2021-12-08 09:25:56 +0100

    Day3

--- adventOfCode/Program.cs
+++ adventOfCode/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 
 namespace adventOfCode
 {
@@ -9,8 +10,9 @@ namespace adventOfCode
 	{
 		static void Main(string[] args)
 		{
-			Day1();
-			Day2();
+			//Day1();
+			//Day2();
+			Day3();
 		}
 
 		public static void Day1()
@@ -165,5 +167,139 @@ namespace adventOfCode
 			return TotalDepth*TotalHorizontal;
 		}
 
+
+		public static void Day3()
+		{
+			var lines = File.ReadAllLines(@"C:\Users\rjl\Documents\adventofcode\input3.txt").ToList();
+			int increasesCount = GetLifeSuppportRating(lines);
+			Console.WriteLine(increasesCount);
+			//increasesCount = GetGammaRate2(lines);
+			//Console.WriteLine(increasesCount);
+		}
+
+		public static int GetLifeSuppportRating(List<string> lines)
+		{
+			//get first bit
+
+
+			GetOxygenGeneratorRating(lines);
+
+			int OxygenRating = GetOxygenGeneratorRating(lines);
+			int CO2Rating = GetCo2Rating(lines);
+
+
+			return OxygenRating * CO2Rating;
+
+		}
+
+		public static int GetCo2Rating(List<string> lines)
+		{
+			int mostCommonBitsLength = GetMostCommonBits(lines).Length;
+			List<string> o2Lines = new List<string>(lines);
+			for (int i = 0; i < mostCommonBitsLength; i++)
+			{
+				int[] mostCommonBits = GetMostCommonBits(o2Lines);
+				if (mostCommonBits[i] == 1)
+				{
+					o2Lines = o2Lines.Where(o => o[i]==('0')).ToList();
+				}
+				else
+				{
+					o2Lines = o2Lines.Where(o => o[i]==('1')).ToList();
+				}
+				if (o2Lines.Count == 1)
+					break;
+
+			}
+			
+
+			string r1 = string.Join("", o2Lines.First());
+			var ans1 = Convert.ToInt32(r1, 2);
+			return ans1;
+
+		}
+
+		public static int GetOxygenGeneratorRating(List<string> lines)
+		{
+			int mostCommonBitsLength = GetMostCommonBits(lines).Length;
+			List<string> o2Lines = new List<string>(lines);
+			for (int i = 0; i < mostCommonBitsLength; i++)
+			{
+				int[] mostCommonBits = GetMostCommonBits(o2Lines);
+				if (mostCommonBits[i] == 1)
+				{
+					o2Lines = o2Lines.Where(o => o[i]==('1')).ToList();
+				}
+				else
+				{
+					o2Lines = o2Lines.Where(o => o[i]==('0')).ToList();
+				}
+
+				if (o2Lines.Count == 1)
+					break;
+			}
+
+			string r1 = string.Join("", o2Lines.First());
+			var ans1 = Convert.ToInt32(r1, 2);
+			return ans1;
+
+		}
+
+		public static int getPowerConsumption(List<string> lines)
+		{
+			var result = GetMostCommonBits(lines);
+			if (result == null) return 0;
+
+			int[] result2 = GetFlippedBits(result);
+
+			string r1 = string.Join("", result);
+			var ans1 = Convert.ToInt32(r1, 2);
+
+			string r = string.Join("", result2);
+			var ans = Convert.ToInt32(r, 2);
+
+			return ans*ans1;
+		}
+
+		private static int[] GetFlippedBits(int[] result)
+		{
+			var result2 = new int[result.Length];
+			for (int j = 0; j < result.Length; j++)
+			{
+				result2[j] = (result[j] == 1 ? 0 : 1);
+			}
+
+			return result2;
+		}
+
+		private static int[] GetMostCommonBits(List<string> lines)
+		{
+			if (!lines.Any()) return null;
+			var length = lines[0].Length;
+			int[] counter = new int[length];
+			Array.Clear(counter, 0, counter.Length);
+			foreach (string line in lines)
+			{
+				for (var j = 0; j < line.Length; j++)
+				{
+					if(line[j] == '1')
+						counter[j] += 1;
+				}
+			}
+
+			int lineCount = lines.Count;
+			int[] result = new int[counter.Length];
+			for (int j = 0; j < counter.Length; j++)
+			{
+				if(counter[j] >= lineCount/2.0)
+					result[j] = 1;
+				else
+				{
+					result[j] = 0;
+				}
+			}
+
+			return result;
+		}
 	}
 }
--- adventOfCodeTests/ProgramTests.cs
+++ adventOfCodeTests/ProgramTests.cs
@@ -47,5 +47,58 @@ namespace adventOfCode.Tests
 			if (result != 900)
 				Assert.Fail();
 		}
+
+		[TestMethod()]
+		public void GetGammaRate2Test()
+		{
+
+			if (Program.GetLifeSuppportRating(input3) != 230)
+				Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void GetGammaRateTest()
+		{
+
+			if (Program.getPowerConsumption(input3) != 198)
+				Assert.Fail();
+		}
+
+		List<string> input3 = new List<string>()
+		{
+			"00100",
+			"11110",
+			"10110",
+			"10111",
+			"10101",
+			"01111",
+			"00111",
+			"11100",
+			"10000",
+			"11001",
+			"00010",
+			"01010"
+		};
+
+		[TestMethod()]
+		public void GetLifeSuppportRatingTest()
+		{
+			if(Program.GetLifeSuppportRating(input3)!=230)
+				Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void GetCo2RatingTest()
+		{
+			if(Program.GetCo2Rating(input3)!=10)
+				Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void GetOxygenGeneratorRatingTest()
+		{
+			if(Program.GetOxygenGeneratorRating(input3)!=23)
+				Assert.Fail();
+		}
 	}
 }
